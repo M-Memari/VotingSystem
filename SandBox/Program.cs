54e3657d47da011fff5166cf295efbae285aac45
Mohammad Ali <memari1989@gmail.com>
2020-05-04 11:29:33 +0430

    created a naive first draft

--- SandBox/Program.cs
+++ SandBox/Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace SandBox
 {
-    class Program
+    internal static class Program
     {
-        static void Main(string[] args)
+        private static void Main()
         {
-            Console.WriteLine("Hello World!");
+            //var ballot = new SortedDictionary<string, int> {{"yesVote", 12}, {"NoCount", 25}, {"Maybe", 25}};
+            //var (option, count) = ballot.ElementAt(0);
+            //Console.WriteLine($"Winner is {option} by {count} votes");
+            //CalculatePercentage(ballot);
         }
+
+        //private static void CalculatePercentage(IDictionary<string, int> ballot)
+        //{
+        //    var total = ballot.Sum(x => x.Value);
+        //    foreach (var option in ballot)
+        //    {
+        //        var percentage = option.Value * 100.0 / total;
+        //        Console.WriteLine($"{option.Key}:{percentage:F}%");
+        //    }
+        //}
     }
 }
